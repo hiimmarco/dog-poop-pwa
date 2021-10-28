e128fe17ef11c9faf63cf375fed88d2c17df17cd
Marco Bogner <marcobogner@gmail.com>
2021-10-28 23:22:57 +0200

    Add fake database

---
+++ pages/_offline.js
@@ -0,0 +1,9 @@
+import Head from 'next/head';
+
+export default function Offline() {
+  return (
+    <div>
+      <h1>You are offline.</h1>
+    </div>
+  );
+}
---
+++ util/database.js
@@ -0,0 +1,34 @@
+const poops = [
+  {
+    id: 1,
+    title: 'Dog Poop at Schwedenplatz',
+    author: 'Max Moser',
+    img: 'url',
+    date: 234,
+    location: 'coordinates',
+  },
+  {
+    id: 2,
+    title: 'Dog Poop at Schwedenplatz',
+    author: 'Max Moser',
+    img: 'url',
+    date: 234,
+    location: 'coordinates',
+  },
+  {
+    id: 3,
+    title: 'Dog Poop at Schwedenplatz',
+    author: 'Max Moser',
+    img: 'url',
+    date: 234,
+    location: 'coordinates',
+  },
+  {
+    id: 4,
+    title: 'Dog Poop at Schwedenplatz',
+    author: 'Max Moser',
+    img: 'url',
+    date: 234,
+    location: 'coordinates',
+  },
+];
