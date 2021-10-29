243aba8ba2b89ac62201038ad2140ff599138a7a
Marco Bogner <marcobogner@gmail.com>
2021-10-29 16:52:30 +0200

    Set pwa to dev mode

--- next.config.js
+++ next.config.js
@@ -4,8 +4,11 @@ module.exports = withPWA({
   reactStrictMode: true,
   pwa: {
     dest: 'public',
-    disable: false,
+    disable: process.env.NODE_ENV === 'development',
     register: true,
     skipWaiting: true,
   },
 });
+
+// Disable service workers for development: process.env.NODE_ENV === 'development'
+// Activate service workers for testing and prod: false
