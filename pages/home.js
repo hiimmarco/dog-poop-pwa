196568e37016f388c54758134a0faf52fd9b3e3d
Marco Bogner <marcobogner@gmail.com>
2021-11-05 15:29:16 +0100

    Add google api package

--- pages/home.js
+++ pages/home.js
@@ -5,8 +5,8 @@ export default function Start(props) {
   return (
     <div>
       <Layout />
-      <div className="flex flex-col h-screen">
-        <div className="mt-8 pl-4 pr-4">
+      <div className="flex flex-col h-screen ">
+        <div className="mt-8 pl-4 pr-4 overflow-y-auto">
           <p className="mb-8 text-2xl font-medium">Recently added</p>
           <div>
             {props.poops.map((poop) => {
