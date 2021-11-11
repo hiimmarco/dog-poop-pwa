0214a1ad5869f7cf54013fc5854a3e3a3800bea2
Marco Bogner <marcobogner@gmail.com>
2021-11-11 19:31:09 +0100

    Add browser location

--- pages/addpoop.js
+++ pages/addpoop.js
@@ -4,7 +4,6 @@ import {
   ComboboxInput,
   ComboboxList,
   ComboboxOption,
-  ComboboxOptionText,
   ComboboxPopover,
 } from '@reach/combobox';
 import { GoogleMap, Marker, useLoadScript } from '@react-google-maps/api';
@@ -33,6 +32,92 @@ const options = {
 
 const libraries = ['places'];
 
+function Locate({ panTo, setLongitude, setLatitude }) {
+  return (
+    <button
+      onClick={() => {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            console.log(position);
+            panTo({
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            });
+            setLatitude(position.coords.latitude);
+            setLongitude(position.coords.longitude);
+          },
+          () => null,
+        );
+      }}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-12 w-12 mb-8"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path
+          fillRule="evenodd"
+          d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
+          clipRule="evenodd"
+        />
+      </svg>
+    </button>
+  );
+}
+
+// Create address bar component with autosuggestions
+function Search({ setLatitude, setLongitude, panTo }) {
+  const {
+    value,
+    suggestions: { status, data },
+    setValue,
+    clearSuggestions,
+  } = usePlacesAutocomplete({
+    requestOptions: {
+      location: { lat: () => 48.208647306552386, lng: () => 16.37347071007108 },
+      radius: 40 * 1000,
+    },
+  });
+  return (
+    <div>
+      <Combobox
+        onSelect={async (address) => {
+          setValue(address, false);
+          clearSuggestions();
+          try {
+            const results = await getGeocode({ address });
+            const { lat, lng } = await getLatLng(results[0]);
+            console.log(lat, lng);
+            setLatitude(lat);
+            setLongitude(lng);
+            panTo({ lat, lng });
+          } catch (error) {
+            console.log('Error');
+          }
+        }}
+      >
+        <ComboboxInput
+          className="mb-8 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+          // placeholder="Enter address"
+        />
+        <ComboboxPopover>
+          <ComboboxList>
+            {status === 'OK' &&
+              data.map(({ place_id, description }) => (
+                <ComboboxOption key={place_id} value={description} />
+              ))}
+          </ComboboxList>
+        </ComboboxPopover>
+      </Combobox>
+    </div>
+  );
+}
+
 export default function Addpoop() {
   const [title, setTitle] = useState('');
   const [markers, setMarkers] = useState({});
@@ -43,20 +128,6 @@ export default function Addpoop() {
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: 'AIzaSyAgZpzR1cuZ1Pe77I8gsJJvKKboJsx_KYk',
     libraries,
-  });
-
-  // Load google places autocomplete
-  const {
-    ready,
-    value,
-    suggestions: { status, data },
-    setValue,
-    clearSuggestions,
-  } = usePlacesAutocomplete({
-    requestOptions: {
-      location: { lat: () => 48.208647306552386, lng: () => 16.37347071007108 },
-      radius: 40 * 1000,
-    },
   });
 
   const onMapClick = useCallback((event) => {
@@ -75,10 +146,10 @@ export default function Addpoop() {
   }, []);
 
   // Create function to pan map to selected location
-  /*   const panTo = useCallback(({ lat, lng }) => {
+  const panTo = useCallback(({ lat, lng }) => {
     mapRef.current.panTo({ lat, lng });
-    mapRef.current.setZoom(14);
-  }, []); */
+    mapRef.current.setZoom(18);
+  }, []);
 
   if (!isLoaded) return 'Loading';
   if (loadError) return 'Load error';
@@ -101,40 +172,16 @@ export default function Addpoop() {
               />
             </label>
             <p className="font-semibold">Address:</p>
-
-            <Combobox
-              onSelect={async (address) => {
-                setValue(address, false);
-                clearSuggestions();
-                try {
-                  const results = await getGeocode({ address });
-                  const { lat, lng } = await getLatLng(results[0]);
-                  console.log(lat, lng);
-                  setLatitude(lat);
-                  setLongitude(lng);
-                } catch (error) {
-                  console.log('Error');
-                }
-              }}
-            >
-              <ComboboxInput
-                className="mb-8 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                value={value}
-                onChange={(e) => {
-                  setValue(e.target.value);
-                }}
-                // placeholder="Enter address"
-              />
-              <ComboboxPopover>
-                <ComboboxList>
-                  {status === 'OK' &&
-                    data.map(({ place_id, description }) => (
-                      <ComboboxOption key={place_id} value={description} />
-                    ))}
-                </ComboboxList>
-              </ComboboxPopover>
-            </Combobox>
-
+            <Search
+              setLatitude={setLatitude}
+              setLongitude={setLongitude}
+              panTo={panTo}
+            />
+            <Locate
+              setLatitude={setLatitude}
+              setLongitude={setLongitude}
+              panTo={panTo}
+            />
             <GoogleMap
               mapContainerStyle={mapContainerStyle}
               zoom={10}
